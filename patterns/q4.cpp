8bfce4ee04ebd2175be28355f22a1bb95d3df858
rohit <paridarohit38@gmail.com>
2024-06-16 22:45:28 +0530

    last commit

--- patterns/q4.cpp
+++ patterns/q4.cpp
@@ -1,15 +1,33 @@
-Problem statement
-Print the following pattern for the given N number of rows.
+// Problem statement
+// Print the following pattern for the given N number of rows.
 
-Pattern for N = 4
-1
-21
-321
-4321
-Detailed explanation ( Input/output format, Notes, Images )
-Input format :
-Integer N (Total no. of rows)
-Output format :
-Pattern in N lines
-Constraints
-0 <= N <= 50
+// Pattern for N = 4
+// 1
+// 21
+// 321
+// 4321
+// Detailed explanation ( Input/output format, Notes, Images )
+// Input format :
+// Integer N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 50
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter the value of N"<<endl;
+    cin>>N;
+    int i=0;
+    for (i ; i<=N;i++){
+        for(int j=i; j>0;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
