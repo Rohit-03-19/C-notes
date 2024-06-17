5c6e614b35a24a2810b92fe81ede2f78caf2d99a
rohit <paridarohit38@gmail.com>
2024-06-17 16:21:42 +0530

    commit

---
+++ patterns/q7.cpp
@@ -0,0 +1,32 @@
+// Problem statement
+// Print the following pattern for the given number of rows.
+
+// Pattern for N = 5
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+// Detailed explanation ( Input/output format, Notes, Images )
+// Input format :
+// N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 26
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    for (int i=1; i<=N;i++){
+        for(int j=i; j>0;j--){
+            cout<<char(70-j);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
